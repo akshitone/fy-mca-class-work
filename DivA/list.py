50fefcb326b8bec492709d483847cd90b69f9458
akshittechbulls <akshit.mithaiwala@techbulls.com>
2023-01-30 08:36:45 +0530

    basics of list

--- DivA/list.py
+++ DivA/list.py
@@ -1,23 +1,69 @@
 # what is list
-numbers = [10, 20, 30, 40, 50]
-print(type(numbers))
+# list: ordered, mutable, allows duplicate elements
+numbers = [10, 20, 30, 40, 50, 60]
+# print(numbers)
+
+# for number in numbers:
+#     print(number)
 
 # indexing
+# print(numbers[0])
+# print(numbers[2])
+# print(numbers[3])
 
 # reverse indexing
+last_element_index = len(numbers)-1
+# print("Last element index:", last_element_index)
+# print("Last element:", numbers[last_element_index])
+
+# print("Last element:", numbers[-1])
 
 # length
+# print(len(numbers))
 
 # delete element
+# print("Before delete:", numbers)
+# del numbers[2]
+# print("After delete:", numbers)
 
 # list remove
+# numbers.remove(40)
+# print("After deleting value 40:", numbers)
+
 
 # pop
+# print("Before pop:", numbers)
+# deleted_value = numbers.pop()
+# print("Deleted value:", deleted_value)
+# print("After pop:", numbers)
 
 # clear
+# numbers.clear()
+# print(numbers)
 
 # append
+# print("Before append:", numbers)
+# numbers.append(100)
+# numbers.append(200)
+# numbers.append(300)
+# print("After append:", numbers)
 
 # extend
+# numbers.extend([1000, 2000, 3000])
+# print("After extend:", numbers)
 
 # insert
+numbers.insert(2, 500)
+print(numbers)
+
+# multiply elements
+zeros = [0]*10
+print(zeros)
+
+# copy list
+
+# append two list
+
+# one liner
+
+# slicing
