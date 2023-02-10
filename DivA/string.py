3dbd2102f28e59a6266b0f3c10bb2e1fab0658fc
akshittechbulls <akshit.mithaiwala@techbulls.com>
2023-02-10 08:56:50 +0530

    String completed till slicing

---
+++ DivA/string.py
@@ -0,0 +1,54 @@
+# string: ordered, ____, text representation
+
+# init
+series_name = "The Woman in the House Across the Street from the Girl in the Window"
+# print(series_name)
+
+# single quote
+fav_quote = 'That\'s what she said'
+# print(fav_quote)
+
+# double quote
+fav_quote = "That's what she said"
+print(fav_quote)
+
+# triple quote
+quote = """Where should I go?
+To the left where nothing is right or
+to the right where nothing is left."""
+# print(quote)
+
+web_series = ["GOT", "HIMYM", "Suits", "Friends", "You", "Elite"]
+
+print(web_series[2:5])
+print(web_series[3:0:-1])
+
+# indexing
+fav_quote = "That's what she said"
+
+
+# reverse string
+print(fav_quote[::-1])
+
+# slicing
+print(fav_quote[-1])
+print(fav_quote[7:15])
+print(fav_quote[10:6:-1])
+
+# concate
+
+# interation
+
+# check
+
+# white space
+
+# upper lower
+
+# startswith
+
+# find
+
+# count
+
+# replace
